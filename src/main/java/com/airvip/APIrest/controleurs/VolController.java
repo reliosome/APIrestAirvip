a2af4fcd7bd7462abaf0caff94a881e6581e0bb9
mohamedfawaz <mohamad.ay.f@gmial.com>
2025-04-02 12:17:50 -0400

    flight

--- src/main/java/com/airvip/APIrest/controleurs/VolController.java
+++ src/main/java/com/airvip/APIrest/controleurs/VolController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "http://localhost:5173\", allowCredentials = \"true")
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RestController
 @RequestMapping("/vols")
 public class VolController {
