b62e992d2b8f2e646306357ff308a39ef1d2ef5f
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-04-03 17:08:46 -0400

    résolution de problème de fichiers

---
+++ src/main/java/com/airvip/APIrest/security/JwtFilter.java
@@ -0,0 +1,55 @@
+package com.airvip.APIrest.security;
+
+import com.airvip.APIrest.repository.UtilisateurRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UtilisateurRepository utilisateurRepository;
+
+    public JwtFilter(JwtUtil jwtUtil, UtilisateurRepository utilisateurRepository) {
+        this.jwtUtil = jwtUtil;
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            String email = jwtUtil.extractEmail(token);
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = new User(email, "", new ArrayList<>());
+
+                if (jwtUtil.validateToken(token)) {
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
